09a6e43a32cefbebe6f4ebdd2eb488976ed66042
han18 <han.fadah@gmail.com>
2024-11-09 22:14:28 -0500

    Working on GLAB 303_5_2

--- ForLoopsGLAB_303_5_1/src/loopdemoone.java
+++ ForLoopsGLAB_303_5_1/src/loopdemoone.java
@@ -43,7 +43,7 @@ public class loopdemoone {
 		       
 		       for(int i=1;i<=5;i++){
 		    	   for(int j=1;j<=i;j++){
-		    		   for(int k=1; k<=j; k++)
+		    		   for(int k=1; k<=j; k++) // i added this code
 		    	       System.out.print("* ");
 		    	   }
 		    	   System.out.println();//new line
