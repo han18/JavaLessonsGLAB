e46aec96908c6615a7ad6ddc394269b1aeb696a7
han18 <han.fadah@gmail.com>
2024-11-11 11:36:00 -0500

    completed working with interfaces

--- Interfaces/src/Movable.java
+++ Interfaces/src/Movable.java
@@ -1,4 +1,9 @@
+public interface Movable {// An interface defines a list of public abstract methods to be implemented by the subclasses
 
-public interface Movable {
-
+   void moveUp();    // "public" and "abstract" by default
+   void moveDown();
+   void moveLeft();
+   void moveRight();
+   String getCoordinate();
 }
+// this is the interfaces that is similar to a class
